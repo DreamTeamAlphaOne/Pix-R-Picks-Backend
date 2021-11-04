ce598b1a3143ab787cfd6c80f622197188bdee68
mhendricks96 <mhendricks96@yahoo.com>
2021-11-03 22:48:26 -0700

    analyzed scripts for emotion scores

---
+++ movie_scores.py
@@ -0,0 +1,52 @@
+
+# Pixar Movies in alphabetical order (except for Luca)
+a_bugs_life_score = {'Happy': 0.08, 'Angry': 0.08, 'Surprise': 0.32, 'Sad': 0.22, 'Fear': 0.29}
+
+brave_score = {'Happy': 0.11, 'Angry': 0.1, 'Surprise': 0.18, 'Sad': 0.28, 'Fear': 0.33}
+
+cars_score = {'Happy': 0.14, 'Angry': 0.06, 'Surprise': 0.31, 'Sad': 0.25, 'Fear': 0.25}
+
+cars_2_score = {'Happy': 0.1, 'Angry': 0.08, 'Surprise': 0.26, 'Sad': 0.22, 'Fear': 0.35}
+
+cars_3_score = {'Happy': 0.12, 'Angry': 0.06, 'Surprise': 0.24, 'Sad': 0.26, 'Fear': 0.31}
+
+coco_score = {'Happy': 0.08, 'Angry': 0.08, 'Surprise': 0.28, 'Sad': 0.23, 'Fear': 0.32}
+
+finding_dory_score = {'Happy': 0.09, 'Angry': 0.09, 'Surprise': 0.27, 'Sad': 0.24, 'Fear': 0.32}
+
+finding_nemo_score = {'Happy': 0.11, 'Angry': 0.09, 'Surprise': 0.25, 'Sad': 0.25, 'Fear': 0.31}
+
+good_dinosaur_score = {'Happy': 0.12, 'Angry': 0.08, 'Surprise': 0.22, 'Sad': 0.2, 'Fear': 0.38}
+
+incredibles_score = {'Happy': 0.1, 'Angry': 0.09, 'Surprise': 0.26, 'Sad': 0.23, 'Fear': 0.33}
+
+incredibles_2_score = {'Happy': 0.13, 'Angry': 0.07, 'Surprise': 0.28, 'Sad': 0.23, 'Fear': 0.29}
+
+inside_out_score = {'Happy': 0.13, 'Angry': 0.06, 'Surprise': 0.21, 'Sad': 0.31, 'Fear': 0.29}
+
+monsters_inc_score = {'Happy': 0.12, 'Angry': 0.08, 'Surprise': 0.29, 'Sad': 0.2, 'Fear': 0.31}
+
+monsters_university_score = {'Happy': 0.12, 'Angry': 0.1, 'Surprise': 0.28, 'Sad': 0.2, 'Fear': 0.3}
+
+onward_score = {'Happy': 0.09, 'Angry': 0.09, 'Surprise': 0.28, 'Sad': 0.2, 'Fear': 0.35}
+
+ratatouille_score = {'Happy': 0.1, 'Angry': 0.08, 'Surprise': 0.25, 'Sad': 0.25, 'Fear': 0.32}
+
+soul_score = {'Happy': 0.11, 'Angry': 0.06, 'Surprise': 0.33, 'Sad': 0.2, 'Fear': 0.3}
+
+toy_story_score = {'Happy': 0.07, 'Angry': 0.08, 'Surprise': 0.36, 'Sad': 0.17, 'Fear': 0.32}
+
+toy_story_2_score = {'Happy': 0.1, 'Angry': 0.05, 'Surprise': 0.4, 'Sad': 0.22, 'Fear': 0.23}
+
+toy_story_3_score = {'Happy': 0.09, 'Angry': 0.09, 'Surprise': 0.28, 'Sad': 0.18, 'Fear': 0.36}
+
+toy_story_4_score = {'Happy': 0.11, 'Angry': 0.08, 'Surprise': 0.31, 'Sad': 0.21, 'Fear': 0.3}
+
+up_score = {'Happy': 0.08, 'Angry': 0.14, 'Surprise': 0.2, 'Sad': 0.22, 'Fear': 0.35}
+
+wall_e_score = {'Happy': 0.07, 'Angry': 0.14, 'Surprise': 0.21, 'Sad': 0.19, 'Fear': 0.39}
+
+#Non-Pixar Movies
+train_dragon_score = {'Happy': 0.06, 'Angry': 0.13, 'Surprise': 0.21, 'Sad': 0.21, 'Fear': 0.39}
+
+frozen_score = {'Happy': 0.11, 'Angry': 0.09, 'Surprise': 0.27, 'Sad': 0.23, 'Fear': 0.3}
